b0ddeafcd337dc070b11fa0ae21686ad25819185
liuhaifeng <liuhaifeng@sensetime.com>
2024-05-15 16:59:10 +0800

    improvement(other):

--- src/views/sse/ansiup.vue
+++ src/views/sse/ansiup.vue
@@ -6,12 +6,12 @@
 			确定
 		</el-button>
 		<div id="ansiupTerminal" class="rounded-[5px] bg-gray-900 overflow-y-auto h-[calc(100%-60px)]">
-			<div
+			<!-- <div
 				v-for="(log, index) in html"
 				:key="index"
 				class="leading-[24px] text-[13px] px-[6px] tracking-[3px] whitespace-pre-wrap break-all log_box"
 				v-html="log"
-			/>
+			/> -->
 		</div>
 	</div>
 </template>
@@ -24,34 +24,48 @@ import { blueLog, greenLog, redLog, whiteLog, yellowLog } from './lib';
 
 
 const recordId = ref('663c4384de3295535b49ede0');
-const html = ref<Array<string>>([]);
+// const html = ref<Array<string>>([]);
 
 const getData = async () => {
 	if (!recordId.value) {
 		return;
 	}
+	const container = document.getElementById('ansiupTerminal');
+
+	if (!container) {
+		return;
+	}
 	const source = new EventSource(`/api/v1/feature/remote-ssh-task/${recordId.value}/log`);
 
 	source.onopen = () => {
-		html.value = [];
+		// html.value = [];
+		container.innerHTML = '';
 	};
 	source.onmessage = (result) => {
 		if (!document.getElementById('ansiupTerminal')) {
 			source.close();
 		}
 		const log = result.data as string;
+		let html = '';
 
 		if (['[start]', '[end]'].some(a => log.includes(a))) {
-			html.value.push(new AnsiUp().ansi_to_html(blueLog(log)));
+			// html.value.push(new AnsiUp().ansi_to_html(blueLog(log)));
+			html = new AnsiUp().ansi_to_html(blueLog(log));
 		} else if (log.includes('[stop]')) {
-			html.value.push(new AnsiUp().ansi_to_html(yellowLog(log)));
+			// html.value.push(new AnsiUp().ansi_to_html(yellowLog(log)));
+			html = new AnsiUp().ansi_to_html(yellowLog(log));
 		} else if (log.includes('[command]')) {
-			html.value.push(new AnsiUp().ansi_to_html(greenLog(log.replace('[command]:', ''))));
+			// html.value.push(new AnsiUp().ansi_to_html(greenLog(log.replace('[command]:', ''))));
+			html = new AnsiUp().ansi_to_html(greenLog(log.replace('[command]:', '')));
 		} else if (log.includes('[error]')) {
-			html.value.push(new AnsiUp().ansi_to_html(redLog(log.replace('[error]:', ''))));
+			// html.value.push(new AnsiUp().ansi_to_html(redLog(log.replace('[error]:', ''))));
+			html = new AnsiUp().ansi_to_html(redLog(log.replace('[error]:', '')));
 		} else {
-			html.value.push(new AnsiUp().ansi_to_html(whiteLog(log)));
+			// html.value.push(new AnsiUp().ansi_to_html(whiteLog(log)));
+			html = new AnsiUp().ansi_to_html(whiteLog(log));
 		}
+		container.innerHTML += `<div class="leading-[24px] text-[13px] px-[6px] tracking-[3px] whitespace-pre-wrap break-all log_box">${html}</div>`;
+
 		if (log.includes('[stop]') || log.includes('[end]')) {
 			source.close();
 		}
